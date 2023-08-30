28df7c73d7643f60ae92c7eb02ed0ee8d0d00dfd
1BM21CS030 <117078161+1BM21CS030@users.noreply.github.com>
2023-08-30 19:49:42 +0530

    Add files via upload

--- knapsack.c
+++ knapsack.c
@@ -1,40 +1,39 @@
 #include<stdio.h>
 
-int a[20][20],w[10],v[10],n,cap;
+struct item{
+    int w;
+    int v;
+};
 
-int sack(){
-    for(int i=0;i<n+1;i++) a[i][0]=0;
-    for(int i=0;i<cap+1;i++) a[0][i]=0;
-    for(int i=1;i<n+1;i++){
-        for(int j=1;j<cap+1;j++){
-             if(w[i-1]>j){
-                a[i][j]=a[i-1][j];
+int n,cap;
+
+struct item a[20];
+
+int knapsack(){
+    int arr[cap+1][n+1];
+    for(int i=0;i<=cap;i++) arr[i][0]=0;
+    for(int i=0;i<=n;i++) arr[0][i]=0;
+
+    for(int i=1;i<=cap;i++){
+        for(int j=1;j<=n;j++){
+            if(i>=a[j-1].w){
+                arr[i][j]=a[j-1].v+arr[i-a[j-1].w][j-1]>arr[i][j-1]?a[j-1].v+arr[i-a[j-1].w][j-1]:arr[i][j-1];
             }else{
-            a[i][j] = a[i-1][j]  >=  (a[i-1][j-w[i-1]]+v[i-1]) ? a[i-1][j] : (a[i-1][j-w[i-1]]+v[i-1]);
+                arr[i][j]=arr[i][j-1];
             }
         }
-
     }
-    //for(int i=0;i<n+1;i++){
-        //for(int j=0;j<cap+1;j++){
-        //    printf("%d ",a[i][j]);
-       // }
-      //  printf("\n");
-  //  }
-    return a[n][cap];
-
-
+    
+    return arr[cap][n];
 }
 
-int main(){
-printf("Enter number of elements\n");
-scanf("%d",&n);
-printf("Enter weight and value\n");
-for(int i=0;i<n;i++){
-    scanf("%d %d",&w[i],&v[i]);
-}
-printf("Enter sack capacity\n");
-scanf("%d",&cap);
-printf("Max profit %d",sack());
-return 0;
+void main(){
+    printf("Enter number of items\n");
+    scanf("%d",&n);
+    printf("Enter weight and value\n");
+    for(int i=0;i<n;i++)
+    scanf("%d %d",&a[i].w,&a[i].v);
+    printf("Enter capacity\n");
+    scanf("%d",&cap);
+    printf("Maximum profits are %d",knapsack());
 }
